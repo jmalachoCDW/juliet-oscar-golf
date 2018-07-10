88cb3779f6575b6d7fb28643fab2492e9113840e
jmalach <jmalach@LT-A0062477.corp.cdw.com>
2018-07-10 15:13:03 -0500

    Updated Golfer and TestGolfer

--- src/main/java/sample/src/Golfer.java
+++ src/main/java/sample/src/Golfer.java
@@ -2,4 +2,29 @@ package sample.src;
 
 public class Golfer {
 
+	private String firstname;
+	private String lastname;
+	private double handicap;
+	
+	public Golfer (String firstname, String lastname, double handicap) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.handicap = handicap;
+		
+	}
+	
+	public String getFirstname () {
+		return firstname;
+		
+	}
+
+	public String getLastname () {
+		return lastname;
+		
+	}
+	
+	public Double getHandicap () {
+		return handicap;
+		
+	}
 }
---
+++ src/test/java/sample/src/TestGolfer.java
@@ -0,0 +1,21 @@
+package sample.src;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestGolfer {
+	
+ @Test
+
+    public void TestGettersandSetters() {
+	Golfer golfer = new Golfer ("joe","malachowski",100);
+
+/* 	String name = golfer.getFirstname(); */
+/* 	String surname = golfer.getLastname(); */
+ 	Double scratch = golfer.getHandicap();
+ 	
+ 	Assert.assertEquals(100.0,scratch);
+ }
+}
